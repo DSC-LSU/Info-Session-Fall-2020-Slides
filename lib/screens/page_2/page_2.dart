908a2e93724fb555c53dc9d4acae212bca715cb3
Rohan Kadkol <rohan-kadkol@users.noreply.github.com>
2020-09-03 17:02:34 -0500

    Completed Page2

--- lib/screens/page_2/page_2.dart
+++ lib/screens/page_2/page_2.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'package:info_session_fall_2020/widgets/default_screen.dart';
 
 class Page2 extends StatelessWidget {
@@ -7,7 +8,43 @@ class Page2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultScreen(
-      child: Container(),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          _GifImage(
+            assetImagePath: 'assets/images/image1.gif',
+          ),
+          _GifImage(
+            assetImagePath: 'assets/images/image2.gif',
+            width: 400.0,
+          ),
+          _GifImage(
+            assetImagePath: 'assets/images/image3.gif',
+          ),
+          _GifImage(
+            assetImagePath: 'assets/images/image4.png',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _GifImage extends StatelessWidget {
+  final String assetImagePath;
+  double height;
+  double width;
+
+  _GifImage({this.assetImagePath, this.height, this.width});
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      assetImagePath,
+      width: width ?? 360.0,
+      height: height ?? 642.0,
+      fit: BoxFit.cover,
     );
   }
 }
--- lib/widgets/default_screen.dart
+++ lib/widgets/default_screen.dart
@@ -17,6 +17,7 @@ class DefaultScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(),
       backgroundColor: Colors.white,
       body: GestureDetector(
         onTap: onTap,
